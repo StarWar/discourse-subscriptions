025fd8d8b18221a6f2ef61d8bcda07ad1ec6177b
Raj Rathore <rajr4thore@gmail.com>
2020-04-23 23:51:08 +0530

    chore(3d-secure): attempt to ask for 3d secure auth

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,31 +10,27 @@ module DiscourseSubscriptions
     requires_login
 
     def create
-      begin
-        customer = DiscourseSubscriptions::Customer.where(user_id: current_user.id, product_id: nil).first_or_create do |c|
-          new_customer = ::Stripe::Customer.create(
-            email: current_user.email
-          )
-
-          c.customer_id = new_customer[:id]
-        end
-
-        payment = ::Stripe::PaymentIntent.create(
-          payment_method_types: ['card'],
-          payment_method: params[:payment_method],
-          amount: params[:amount],
-          currency: params[:currency],
-          customer: customer[:customer_id],
-          confirm: true
+      customer = DiscourseSubscriptions::Customer.where(user_id: current_user.id, product_id: nil).first_or_create do |c|
+        new_customer = ::Stripe::Customer.create(
+          email: current_user.email
         )
 
-        render_json_dump payment
-
-      rescue ::Stripe::InvalidRequestError => e
-        render_json_error e.message
-      rescue ::Stripe::CardError => e
-        render_json_error I18n.t('discourse_subscriptions.card.declined')
+        c.customer_id = new_customer[:id]
       end
+
+      payment = ::Stripe::PaymentIntent.create(
+        payment_method_types: ["card"],
+        payment_method: params[:payment_method],
+        amount: params[:amount],
+        currency: params[:currency],
+        customer: customer[:customer_id]
+      )
+
+      render_json_dump payment
+    rescue ::Stripe::InvalidRequestError => e
+      render_json_error e.message
+    rescue ::Stripe::CardError => e
+      render_json_error I18n.t("discourse_subscriptions.card.declined")
     end
   end
 end
